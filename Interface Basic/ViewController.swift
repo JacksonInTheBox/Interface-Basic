185bcdd40bff58dd4de664d018f46691dda58d9d
Jackson <JacksonCokeNJ@gmail.com>
2023-09-19 14:19:04 -0400

    .

--- Interface Basic/ViewController.swift
+++ Interface Basic/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var label1: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func greet(_ sender: Any) {
+        label1.text = textField.text
+    }
+    
 }
 
